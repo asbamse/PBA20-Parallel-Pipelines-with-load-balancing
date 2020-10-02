128c4769268975671026d918d49df0309809b754
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2020-10-02 11:28:54 +0200

    Initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PBA20_Parallel_Pipelines_with_load_balancing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
