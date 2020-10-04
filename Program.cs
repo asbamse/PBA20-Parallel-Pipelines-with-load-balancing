b95f42cccbac2120ddcbcb6247021a5df58b0a7a
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2020-10-04 10:13:03 +0200

    MeasureTime method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace PBA20_Parallel_Pipelines_with_load_balancing
 {
@@ -7,6 +8,18 @@ namespace PBA20_Parallel_Pipelines_with_load_balancing
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Measures action and prints the result to console.
+        /// </summary>
+        /// <param name="p">Action to measure.</param>
+        private static void MeasureTime(Action p)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            p.Invoke();
+            sw.Stop();
+            Console.WriteLine("Time = {0:F5} sec.", sw.ElapsedMilliseconds / 1000d);
         }
     }
 }
