9bce9471a3d613e8e1739541b6a090e1085618d7
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2020-10-04 10:42:54 +0200

    Simple appsettings load

--- Program.cs
+++ Program.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 
 namespace PBA20_Parallel_Pipelines_with_load_balancing
 {
     class Program
     {
+        private static string InputDirectory { get; set; }
+        private static string OutputDirectory { get; set; }
+
         static void Main(string[] args)
         {
+            LoadAppSettings();
+
             Console.WriteLine("Executing all operations sequentially.");
             MeasureTime(ExecuteSequentialOperation);
             Console.WriteLine("Finished executing all operations sequentially.");
+        }
+
+        /// <summary>
+        /// Loads application settings from the appsettings.json
+        /// </summary>
+        private static void LoadAppSettings()
+        {
+            string path = $"{Directory.GetParent(AppContext.BaseDirectory).FullName}{Path.DirectorySeparatorChar}appsettings.json";
+            JObject o1 = JObject.Parse(File.ReadAllText(path));
+            InputDirectory = o1.Value<string>("InputDirectory");
+            Console.WriteLine("Input from: " + InputDirectory);
+            OutputDirectory = o1.Value<string>("OutputDirectory");
+            Console.WriteLine("Output from: " + OutputDirectory);
         }
 
         /// <summary>
