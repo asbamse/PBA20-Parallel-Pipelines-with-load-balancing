f0b27434ca0e35a931b9af56bd2739e042b7e6a9
JanvanZetten <janx3779@easv365.dk>
2020-10-09 16:13:10 +0200

    Apperently forgot to save the file before commiting

--- IPipelineStep.cs
+++ IPipelineStep.cs
@@ -3,5 +3,22 @@ namespace PBA20_Parallel_Pipelines_with_load_balancing
 {
     public interface IPipelineStep
     {
+        // Input queue
+        //    V
+        //  Internal Multiple Tasks
+        //    V
+        //  Internal Multiple TaskQueue
+        //    V
+        //  Internal Multiplexor
+        //    V
+        // Output queue
+
+        // Constructor InputQueue, OutputQueue, Action
+        // TODO Find out how to add the TaskQueue til Action
+
+        bool AddTask();
+        bool RemoveTask(); // Only return after Task Removal
+        int TaskAmount();
+        int QueueFillLevel();
     }
 }
